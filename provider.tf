1d0deb1b60655f30e6bb8b4366f673aeb60f2587
mcitlynandres <126923637+mcitlynandres@users.noreply.github.com>
2023-03-24 20:34:54 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,14 +8,14 @@ terraform {
 }
 
 provider "google" {
-  project     = "avian-amulet-378416"
-  region      = "us-central1"
+  project     = "lynalpha"
+  region      = "us-east4"
 }
 
 
 terraform {
   cloud {
-    organization = "sami123123123"
+    organization = "lynalpha"
 
     workspaces {
       name = "mobility-base-storage"
